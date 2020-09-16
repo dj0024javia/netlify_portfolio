3ec765972065b27b622227f7492a98e17f6a31e1
dj0024javia <31767853+dj0024javia@users.noreply.github.com>
2020-09-16 17:48:33 +0530

    build folder added

---
+++ react-portfolio-master/build/precache-manifest.032ac96a043e8008620d3f40f27de552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d5f41cb18ec1c6e499c6143cf4be17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff6514f05dac2b9169ac",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6514f05dac2b9169ac",
+    "url": "/static/js/main.0130df0e.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
